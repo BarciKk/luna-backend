76a5734fe18dcbffdfc3cae3b93ef9a48953cc0c
BarciKk <kamil.bartuzel2@o2.pl>
2023-09-01 00:18:16 +0200

    removed console logs from config

--- excercise-app/Backend/config/config.js
+++ excercise-app/Backend/config/config.js
@@ -1,9 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT", process.env.PORT);
-console.log("URI", process.env.MONGO_URI);
-
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI;
 
