4bf2563c952e1d85a494f1e3b238c7d49c1d6920
BarciKk <kamil.bartuzel2@o2.pl>
2023-11-19 00:49:32 +0100

    pushing files into repo

--- excercise-app/Backend/src/workout/workout.routes.js
+++ excercise-app/Backend/src/workout/workout.routes.js
@@ -8,19 +8,14 @@ import {
 } from "./workout.controller.js";
 const workoutRouter = Router();
 
-//get workouts
 workoutRouter.get("/", getAllWorkouts);
 
-//get single workout
 workoutRouter.get("/:id", getSingleWorkout);
 
-//create new workout
 workoutRouter.post("/", createWorkout);
 
-//delete single workout
 workoutRouter.delete("/:id", deleteWorkout);
 
-//update  workout
 workoutRouter.patch("/:id", updateWorkout);
 
 export { workoutRouter };
