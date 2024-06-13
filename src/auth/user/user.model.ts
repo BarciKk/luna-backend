f3bd2e07aaf5ae9b5266fa49294226926ae49030
BarciKk <kamil.bartuzel2@o2.pl>
2024-06-13 19:30:41 +0200

    added default avatar while user register

--- src/auth/user/user.model.ts
+++ src/auth/user/user.model.ts
@@ -1,7 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 import { User } from "../../types/user/index";
 
-
 const userSchema = new Schema<User>({
   username: {
     type: String,
@@ -15,6 +14,8 @@ const userSchema = new Schema<User>({
   },
   avatar: {
     type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ_ywnvKwB32lJoD9rnf9M-YqSI0FvEYiJx8g&s",
   },
   isActive: {
     type: Boolean,
